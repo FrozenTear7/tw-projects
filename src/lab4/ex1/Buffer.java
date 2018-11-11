e7ab94c9d366d8415bb5bc78a2eca31d14556df8
FrozenTear7 <RainbowPablo7@gmail.com>
2018-11-11 12:56:30 +0100

    Add list of locks and conditions

--- src/lab4/ex1/Buffer.java
+++ src/lab4/ex1/Buffer.java
@@ -12,73 +12,83 @@ import static lab4.ex1.ex4_1.middlemanSize;
 public class Buffer {
     private int size;
     private List<Integer> itemList = new ArrayList<>(size);
-    private Lock lock = new ReentrantLock();
-    private int writeIndex = 0;
-    private int readIndex = 0;
-    private Condition myIndexCondition = lock.newCondition();
+    private List<Lock> lockList = new ArrayList<>(size);
+    private List<Condition> conditionList = new ArrayList<>(size);
+    private List<Integer> indexList = new ArrayList<>(middlemanSize + 2);
 
     public Buffer(int size) {
         this.size = size;
 
         for (int i = 0; i < size; i++) {
             itemList.add(i, -1);
+            lockList.add(i, new ReentrantLock());
+            conditionList.add(i, lockList.get(i).newCondition());
+            indexList.add(i, 0);
         }
     }
 
     public void produce() {
-        lock.lock();
+        int lockIndex = indexList.get(0);
+
+        lockList.get(lockIndex).lock();
 
         try {
-            while(itemList.get(writeIndex) != -1)
-                myIndexCondition.await();
+            while(itemList.get(indexList.get(0)) != -1)
+                conditionList.get(lockIndex).await();
 
-            itemList.set(writeIndex, 0);
+            itemList.set(indexList.get(0), 0);
 
-            writeIndex = ++writeIndex % size;
+            indexList.set(0, (indexList.get(0) + 1) % size);
 
-            myIndexCondition.signalAll();
+            conditionList.get(lockIndex).signalAll();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
-            lock.unlock();
+            lockList.get(lockIndex).unlock();
         }
     }
 
     public void consume() {
-        lock.lock();
+        int lockIndex = indexList.get(middlemanSize + 1);
+
+        lockList.get(lockIndex).lock();
 
         try {
-            while(itemList.get(readIndex) != middlemanSize)
-                myIndexCondition.await();
+            while(itemList.get(indexList.get(middlemanSize + 1)) != middlemanSize)
+                conditionList.get(lockIndex).await();
 
-            System.out.println("Item at: " + readIndex + " = " + itemList.get(readIndex));
+            System.out.println("Item at: " + indexList.get(middlemanSize + 1) + " = " + itemList.get(indexList.get(middlemanSize + 1)));
 
-            itemList.set(readIndex, -1);
+            itemList.set(indexList.get(middlemanSize + 1), -1);
 
-            readIndex = ++readIndex % size;
+            indexList.set(middlemanSize + 1, (indexList.get(middlemanSize + 1) + 1) % size);
 
-            myIndexCondition.signalAll();
+            conditionList.get(lockIndex).signalAll();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
-            lock.unlock();
+            lockList.get(lockIndex).unlock();
         }
     }
 
     public void modify(int id) {
-        lock.lock();
+        int lockIndex = indexList.get(id + 1);
+
+        lockList.get(lockIndex).lock();
 
         try {
-            while(itemList.get(readIndex) != id)
-                myIndexCondition.await();
+            while(itemList.get(indexList.get(id + 1)) != id)
+                conditionList.get(lockIndex).await();
 
-            itemList.set(readIndex, id + 1);
+            itemList.set(indexList.get(id + 1), id + 1);
 
-            myIndexCondition.signalAll();
+            indexList.set(id + 1, (indexList.get(id + 1) + 1) % size);
+
+            conditionList.get(lockIndex).signalAll();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
-            lock.unlock();
+            lockList.get(lockIndex).unlock();
         }
     }
 }
